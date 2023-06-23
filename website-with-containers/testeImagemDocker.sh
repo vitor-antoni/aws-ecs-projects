8bea88892585e9947791add8175a79143062c675
Vitor Antoni <v.silva.antoni@gmail.com>
2023-06-23 12:01:20 -0300

    Update testeImagemDocker.sh

--- website-with-containers/testeImagemDocker.sh
+++ website-with-containers/testeImagemDocker.sh
@@ -5,9 +5,19 @@ systemctl start docker
 systemctl enable docker
 
 # Vamos criar um diretório para trabalho
-mkdir /aplicacao
-cd /aplicacao
+#    mkdir /website-with-containers
+#    cd /website-with-containers
 
 # Criar um diretorio para hospedar os arquivos do site
-mkdir aplicacao-web
-touch aplicacao-web/index.html
+#    mkdir html
+#    touch html/index.html
+
+# Cria uma imagem com base no Dockerfile
+#   "-t" → para especificar uma tag
+docker image build -t aplicacao-web:versao0.1 .
+docker image ls
+
+# Rodar o container
+#   "-d" → para rodar em background
+#   "-p" → para especificar a porta
+docker container run -d -p 80:80 aplicacao-web:versao0.1
