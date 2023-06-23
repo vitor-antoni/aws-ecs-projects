3ada9e28693ee7816b94cd06c81143e1d8ff448d
Vitor Antoni <v.silva.antoni@gmail.com>
2023-06-23 12:04:51 -0300

    Update enviaParaECR.sh

--- website-with-containers/enviaParaECR.sh
+++ website-with-containers/enviaParaECR.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# aws configure set aws_access_key_id <access_key>
-# aws configure set aws_secret_access_key <secret_key>
-# aws configure set aws_session_token <session_token>
+# Se necessário, precisará especificar as credencias da função para a instância.
+#   aws configure set aws_access_key_id <access_key>
+#   aws configure set aws_secret_access_key <secret_key>
+#   aws configure set aws_session_token <session_token>
 
-aws ecr get-login-password --region <regiao> | docker login --username AWS --password-stdin <id_conta_aws>.dkr.ecr.<regiao>.amazonaws.com
+aws ecr get-login-password --region [regiao] | docker login --username AWS --password-stdin [id_conta_aws].dkr.ecr.[regiao].amazonaws.com
 
-docker build -t app-image .
-docker tag app-image:latest <id_conta_aws>.dkr.ecr.<regiao>.amazonaws.com/app-image:latest
 
-docker push <id_conta_aws>.dkr.ecr.<regiao>.amazonaws.com/app-image:latest
+docker tag aplicacao-web:versao0.1 [id_conta_aws].dkr.ecr.[regiao].amazonaws.com/repo-aplicacao-web:versao0.1
+
+docker push [id_conta_aws].dkr.ecr.[regiao].amazonaws.com/repo-aplicacao-web:versao0.1
